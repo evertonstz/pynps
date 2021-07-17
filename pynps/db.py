2e6913bc96d8c22f7f934f6f60610f58cd65e0e2
evertonstz <evertonjcorreia@gmail.com>
2021-07-16 22:47:16 -0300

    add some properties and a __enter__/__exit__ pair to Database class

--- pynps/db.py
+++ pynps/db.py
@@ -31,14 +31,68 @@ class Database:
     table: str
 
     def __post_init__(self):
-        self.con = sqlite3.connect(self.path)
-        self.con.row_factory = self._dict_factory
+        self._conn = sqlite3.connect(self.path)
+        self._conn.row_factory = self._query_factory
+        self._cursor = self._conn.cursor()
 
         # TODO check if table exists, if not, create it
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    @property
+    def connection(self):
+        return self._conn
+
+    @property
+    def cursor(self):
+        return self._cursor
+
+    def _query_factory(self, cursor: Cursor, row: tuple) -> dict[str, Any]:
+        """this is used by connection's row_factory method so it knows every row should return as a dict instead of atuple"""
+        d = {}
+        for idx, col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
+        return d
+
+    def _query_to_games(self, query: Cursor):
+        """this is used to convert the list of dicts that results from a query into a list of Game objects"""
+        return [
+            Game(
+                game_id=row["game_id"],
+                platform=row["platform"],
+                type=row["type"],
+                region=row["region"],
+                name=row["name"],
+                pkg_direct_link=row["pkg_direct_link"],
+                zrif=row["zrif"],
+                content_id=row["content_id"],
+                last_modified_date=row["last_modified_date"],
+                original_name=row["original_name"],
+                sha256=row["sha256"],
+                file_size=row["file_size"],
+                app_version=row["app_version"],
+                required_fw=row["required_fw"],
+                rap=row["rap"],
+                rap_direct_link=row["rap_direct_link"],
+            )
+            for row in query
+        ]
+
+    def close(self) -> None:
+        self._conn.close()
+
+    def commit(self) -> None:
+        self._conn.commit()
+
+    def rollback(self) -> None:
+        self._conn.rollback()
+
     def touch_table(self) -> None:
-        cur = self.con.cursor()
-        cur.execute(
+        self._cursor.execute(
             f"""
         CREATE TABLE IF NOT EXISTS {self.table} (
 		    Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
@@ -61,48 +115,9 @@ class Database:
 	    );"""
         )
 
-    def close(self) -> None:
-        self.con.close()
-
-    def commit(self) -> None:
-        self.con.commit()
-
-    def rollback(self) -> None:
-        self.con.rollback()
-
-    def _dict_factory(self, cursor: Cursor, row: tuple) -> dict[str, Any]:
-        d = {}
-        for idx, col in enumerate(cursor.description):
-            d[col[0]] = row[idx]
-        return d
-
     def query_all(self) -> list[Game]:
-        cur = self.con.cursor()
-        query = cur.execute(f"""SELECT * FROM {self.table};""")
-
-        res = [
-            Game(
-                game_id=row["game_id"],
-                platform=row["platform"],
-                type=row["type"],
-                region=row["region"],
-                name=row["name"],
-                pkg_direct_link=row["pkg_direct_link"],
-                zrif=row["zrif"],
-                content_id=row["content_id"],
-                last_modified_date=row["last_modified_date"],
-                original_name=row["original_name"],
-                sha256=row["sha256"],
-                file_size=row["file_size"],
-                app_version=row["app_version"],
-                required_fw=row["required_fw"],
-                rap=row["rap"],
-                rap_direct_link=row["rap_direct_link"],
-            )
-            for row in query
-        ]
-
-        return res
+        query = self._cursor.execute(f"""SELECT * FROM {self.table};""")
+        return self._query_to_games(query)
 
 
 @dataclass
@@ -114,7 +129,7 @@ class Resumes(Database):
 class GameDatabase(Database):
     def upsert(self, game: Game) -> None:
         """this method upserts the data from a Game object into the sqlite3 database"""
-        cur = self.con.cursor()
+        cur = self._conn.cursor()
         t = cur.execute(
             f"""
 		INSERT INTO {self.table} (game_id, platform, type, region, name, pkg_direct_link, zrif, content_id, last_modified_date, original_name, sha256, file_size, app_version, required_fw, rap, rap_direct_link)
